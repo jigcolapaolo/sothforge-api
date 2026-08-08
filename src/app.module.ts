dc7845aa5a86aff91d5558fd172ff64812d31862
Juan <Jinbyos@Gmail.com>
2026-08-08 03:28:33 -0300

    chore: created initial folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { ProjectsModule } from './projects/projects.module';
+import { BoardsModule } from './boards/boards.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { LabelsModule } from './labels/labels.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { DatabaseModule } from './database/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, OrganizationsModule, ProjectsModule, BoardsModule, TasksModule, CommentsModule, LabelsModule, NotificationsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
