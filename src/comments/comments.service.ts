c76ea1a00a66f8002203c8225a876099f8b003c2
Juan <Jinbyos@Gmail.com>
2026-09-03 21:37:16 -0300

    feat: added create comment endpoint

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,7 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
 export class CommentsService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async create(taskId: string, authorId: string, dto: CreateCommentDto) {
+    return this.prisma.comment.create({
+      data: {
+        taskId,
+        authorId,
+        content: dto.content,
+      },
+      select: {
+        id: true,
+        content: true,
+        createdAt: true,
+        updatedAt: true,
+        author: {
+          select: {
+            id: true,
+            username: true,
+            avatar: true,
+          },
+        },
+      },
+    });
+  }
 }
--- src/comments/controllers/task-comments.controller.ts
+++ src/comments/controllers/task-comments.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { CommentsService } from '../comments.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { TaskGuard } from 'src/tasks/guards/task.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { OrganizationRole } from 'src/generated/prisma/enums';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import type { AuthenticatedUser } from 'src/auth/types/authenticated-user';
+import { CreateCommentDto } from '../dto/create-comment.dto';
 
 @Controller('tasks/:taskId/comments')
 export class TaskCommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, TaskGuard, RolesGuard)
+  @Roles(
+    OrganizationRole.OWNER,
+    OrganizationRole.ADMIN,
+    OrganizationRole.MEMBER,
+  )
+  create(
+    @Param('taskId') taskId: string,
+    @CurrentUser() user: AuthenticatedUser,
+    @Body() dto: CreateCommentDto,
+  ) {
+    return this.commentsService.create(taskId, user.userId, dto);
+  }
 }
