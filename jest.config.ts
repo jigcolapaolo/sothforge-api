fcac5d5b633e4481c9b1d9fa66293abefc22ba06
Juan <Jinbyos@Gmail.com>
2026-09-10 21:38:18 -0300

    test: configure Jest testing environment and deleted all unused tests

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: 'test/.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: 'coverage',
+};
+
+export default config;
